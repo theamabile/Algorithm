8ece20b7a510a6f0dff9f7b91f748bd789531bf5
JeongHyeJIn <theambabile@gmail.com>
2021-05-22 20:18:35 +0900

    [프로그래머스] 여행경로 주석 제거 및 코드 정리

--- src/프로그래머스/DFS와BFS/여행경로/Solution.java
+++ src/프로그래머스/DFS와BFS/여행경로/Solution.java
@@ -1,31 +1,12 @@
 /**
- * KaKao Programmers / BaekJoon
+ * KaKao Programmers 
  * Category : 프로그래머스.DFS와BFS.여행경로
- * Date : 2021. 5. 16.
+ * Date : 2021. 5. 22 
  * User : theamabile@gmail.com
  */
 package 프로그래머스.DFS와BFS.여행경로;
 import java.util.ArrayList;
 import java.util.Collections;
-
-
-/* 1. ICN 미리 추가
-
----DFS
-0. ICN을 answer에 추가
-1. answer의 갯수가 ticket.length+1이면 (다 도착했쓰)
-   break
-   
-2. ICN으로 갈 수 있는 목록을 찾아서 todo에 저장
-3. todo sort
-// for todo 수 만큼
-//   if(visited가 false면 (아직 방문 안했음) )
-//       visited[i] = ture;
-         DFS(ticket[i][1] // 목적지, visited )
-         visited[i] = false;
-
-*/
-
 
 class Solution {
     
@@ -64,9 +45,6 @@ class Solution {
         
         answer.add(current);
         
-        // 현재 위치에서 방문 가능한 목록을 오름차순으로 정렬한 다음 반복 시키기
-        // 모두 방문 가능한 경로가 1개 이상 일 때
-        //   -> 인덱스가 앞쪽인 티켓이 정답에 가까운 티켓임.(도착지 이름 순 정렬)
         ArrayList<Ticket> todo = new ArrayList<Ticket>();
         for(int i=0 ; i<tickets.length ; i++) {
             if(!visited[i] && tickets[i][0].equals(current)) {
@@ -75,26 +53,17 @@ class Solution {
         }
         Collections.sort(todo);
         
-        System.out.println(" current : "+current);
-        for(Ticket t : todo) {
-             System.out.println("      > "+t.dest);
-        }     
-        
-        // 방문 가능한 곳이 없음 = 정상적으로 끝났거나 더이상 갈 곳이 없는 잘못된 경로
         if(todo.size() <= 0) {  
             return;
         }
-                         
-        // 알파벳 순서가 앞이라서 갔지만 그 경로가 모두 방문 할 수 있는 경로가 아닐 수도 있음
-        //   -> 현재 위치에서 갈 수 있는 곳 모두 가보기
+
         for(Ticket t : todo) {
             visited[t.index] = true;
             DFS(t.dest, visited);
             visited[t.index] = false;  
             
-            //  DFS는 끝까지 들어갔다가 오는 것, 여기서 만들어진 경로가 tickets.length+1개가 아니면 잘못된 경로임 
             if(answer.size() != tickets.length+1) {     
-                answer.remove(answer.size()-1);     // 제거
+                answer.remove(answer.size()-1); 
             }   
         }               
         
